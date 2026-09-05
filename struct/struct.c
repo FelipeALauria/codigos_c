6466aef67161614c01a2d211fe853de5a8463409
Felipe Lauria <fel.lauria@gmail.com>
2026-09-05 00:29:37 -0300

    structs... structs... structs

--- struct/struct.c
+++ struct/struct.c
@@ -1,7 +1,25 @@
 #include "structs_functions.h"
+#include <stdio.h>
 
 #define N 100
 
-void main () {
-    printf("Hello world! %n", N);
+struct materia;
+
+typedef struct {
+    char* nome;
+    int idade;
+    int ra;
+    struct materia* materia_atual;  
+} aluno;
+
+typedef struct {
+    char* professor;
+    int num_alunos;
+    char* departamento;
+    aluno* alunos;  
+} materia;
+
+int main(void) {
+    printf("Hello world! %d\n", N);
+    return 0;
 }
--- struct/structs_functions.h
+++ struct/structs_functions.h
@@ -1,1 +1,17 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+double calculo_nota_media_final_pesos(double* peso, double* notas){
+    double notas_x_pesos = 0.0, soma_pesos = 0.0;
+    
+    for(int i = 0; i < sizeof(notas); i++){
+        notas_x_pesos += notas[i] * peso[i];
+        soma_pesos += peso[i];
+    }
+
+    double media = 0.0;
+
+    return media = notas_x_pesos/soma_pesos;
+}
+
